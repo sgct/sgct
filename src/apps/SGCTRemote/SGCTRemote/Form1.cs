0351d028a5853b409c77e25c628ea63d110a072c
Miroslav Andel <miroslav.andel@gmail.com>
2012-08-23 20:56:40 +0000

    Added external c# control sample.

---
+++ src/apps/SGCTRemote/SGCTRemote/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SGCTRemote
+{
+    struct clientData
+    {
+        public NetworkManager connection;
+        public string ip;
+        public Int32 port;
+        public int bufferSize;
+    }
+    
+    public partial class Form1 : Form
+    {
+        private clientData c;
+        private bool connected;
+
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        private void init()
+        {
+            connected = false;
+
+            c = new clientData();
+            c.connection = new NetworkManager();
+            c.port = 20500;
+            c.ip = "127.0.0.1"; //default ip
+            c.bufferSize = 1024;
+
+            this.toolStripStatusLabel1.Text = "Disconnected";
+            this.ipTextBox.Text = c.ip;
+            this.SizeTrackBar.Value = 50;
+            this.SizeLabel.Text = "Size = 50 %";
+
+            componentVisibility(false);
+        }
+
+        private void exit()
+        {
+            disconnect();
+            System.Environment.Exit(0);
+        }
+
+        /*
+         * Enable or disable items depending on connection status
+         */
+        private void componentVisibility(bool status)
+        {
+            this.PropertiesGroupBox.Enabled = status;
+        }
+
+        #region Network
+
+        private void connect()
+        {
+            //if connection successfull
+            if (c.connection.ConnectIP(c.ip, c.port, c.bufferSize))
+            {
+                connected = true;
+                c.connection.alive = true;
+                componentVisibility(true);
+                this.connectButton.Text = "Disconnect";
+                this.toolStripStatusLabel1.Text = "Connected";
+                
+                //send defaults
+                //c.connection.Send(
+            }
+            else
+            {
+                componentVisibility(false);
+                this.connectButton.Text = "Connect";
+                this.toolStripStatusLabel1.Text = "Disconnected";
+                MessageBox.Show("Failed to connect to server!");
+            }
+        }
+
+        private void disconnect()
+        {
+            componentVisibility(false);
+            this.connectButton.Text = "Connect";
+            this.toolStripStatusLabel1.Text = "Disconnected";
+
+            if (c.connection != null)
+            {
+                connected = false;
+                c.connection.Disconnect();
+            }
+        }
+
+        #endregion
+
+        private void MainForm_Closed(object sender, System.EventArgs e)
+        {
+            exit();
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            if (!connected)
+            {
+                c.ip = this.ipTextBox.Text;
+                connect();
+            }
+            else
+            {
+                disconnect();
+            }
+        }
+
+        private void StatsCheckBox_CheckedChanged(object sender, EventArgs e)
+        { 
+            if (connected)
+            {
+                CheckBox cb = (CheckBox)sender;
+
+                if (cb.Checked)
+                    c.connection.Send("stats=1");
+                else
+                    c.connection.Send("stats=0");
+            }
+        }
+
+        private void GraphCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (connected)
+            {
+                CheckBox cb = (CheckBox)sender;
+
+                if (cb.Checked)
+                    c.connection.Send("graph=1");
+                else
+                    c.connection.Send("graph=0");
+            }
+        }
+
+        private void WireframeCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (connected)
+            {
+                CheckBox cb = (CheckBox)sender;
+
+                if (cb.Checked)
+                    c.connection.Send("wire=1");
+                else
+                    c.connection.Send("wire=0");
+            }
+        }
+
+        private void SizeTrackBar_Scroll(object sender, EventArgs e)
+        {
+            TrackBar tb = (TrackBar)sender;
+            this.SizeLabel.Text = "Size: " + tb.Value.ToString() + " %";
+
+            if (connected)
+            {
+                c.connection.Send("size=" + tb.Value.ToString());
+            }
+        }
+    }
+}
---
+++ src/apps/SGCTRemote/main.cpp
@@ -0,0 +1,127 @@
+#include "sgct.h"
+
+sgct::Engine * gEngine;
+
+void myDrawFun();
+void myPreSyncFun();
+void myPostSyncPreDrawFun();
+void myEncodeFun();
+void myDecodeFun();
+
+void externalControlCallback(const char * receivedChars, int size, int clientId);
+
+double curr_time = 0.0;
+float size_factor = 0.5f;
+
+bool showStats = false;
+bool showGraph = false;
+bool showWireframe = false;
+
+int main( int argc, char* argv[] )
+{
+
+	// Allocate
+	gEngine = new sgct::Engine( argc, argv );
+
+	// Bind your functions
+	gEngine->setDrawFunction( myDrawFun );
+	gEngine->setPreSyncFunction( myPreSyncFun );
+	gEngine->setPostSyncPreDrawFunction( myPostSyncPreDrawFun );
+	gEngine->setExternalControlCallback( externalControlCallback );
+
+	sgct::SharedData::Instance()->setEncodeFunction(myEncodeFun);
+	sgct::SharedData::Instance()->setDecodeFunction(myDecodeFun);
+
+	// Init the engine
+	if( !gEngine->init() )
+	{
+		delete gEngine;
+		return EXIT_FAILURE;
+	}
+
+	// Main loop
+	gEngine->render();
+
+	// Clean up (de-allocate)
+	delete gEngine;
+
+	// Exit program
+	exit( EXIT_SUCCESS );
+}
+
+void myDrawFun()
+{
+	float speed = 50.0f;
+	glRotatef(static_cast<float>( curr_time ) * speed, 0.0f, 1.0f, 0.0f);
+
+	//render a single triangle
+	glBegin(GL_TRIANGLES);
+		glColor3f(1.0f, 0.0f, 0.0f); //Red
+		glVertex3f(-0.5f * size_factor, -0.5f * size_factor, 0.0f);
+
+		glColor3f(0.0f, 1.0f, 0.0f); //Green
+		glVertex3f(0.0f, 0.5f * size_factor, 0.0f);
+
+		glColor3f(0.0f, 0.0f, 1.0f); //Blue
+		glVertex3f(0.5f * size_factor, -0.5f * size_factor, 0.0f);
+	glEnd();
+}
+
+void myPreSyncFun()
+{
+	//set the time only on the master
+	if( gEngine->isMaster() )
+	{
+		//get the time in seconds
+		curr_time = sgct::Engine::getTime();
+	}
+}
+
+void myPostSyncPreDrawFun()
+{
+	gEngine->setDisplayInfoVisibility( showStats );
+	gEngine->setStatsGraphVisibility( showGraph );
+	gEngine->setWireframe( showWireframe );
+}
+
+void myEncodeFun()
+{
+	sgct::SharedData::Instance()->writeDouble( curr_time );
+	sgct::SharedData::Instance()->writeFloat( size_factor );
+	sgct::SharedData::Instance()->writeBool( showStats );
+	sgct::SharedData::Instance()->writeBool( showGraph );
+	sgct::SharedData::Instance()->writeBool( showWireframe );
+}
+
+void myDecodeFun()
+{
+	curr_time = sgct::SharedData::Instance()->readDouble();
+	size_factor = sgct::SharedData::Instance()->readFloat();
+	showStats = sgct::SharedData::Instance()->readBool();
+	showGraph = sgct::SharedData::Instance()->readBool();
+	showWireframe = sgct::SharedData::Instance()->readBool();
+}
+
+void externalControlCallback(const char * receivedChars, int size, int clientId)
+{
+	if( gEngine->isMaster() )
+	{
+		if(size == 7 && strncmp(receivedChars, "stats", 5) == 0)
+		{
+			showStats = (strncmp(receivedChars + 6, "1", 1) == 0);
+		}
+		else if(size == 7 && strncmp(receivedChars, "graph", 5) == 0)
+		{
+			showGraph = (strncmp(receivedChars + 6, "1", 1) == 0);
+		}
+		else if(size == 6 && strncmp(receivedChars, "wire", 4) == 0)
+		{
+			showWireframe = (strncmp(receivedChars + 5, "1", 1) == 0);
+		}
+		else if(size >= 6 && strncmp(receivedChars, "size", 4) == 0)
+		{
+			int tmpVal = atoi(receivedChars + 5);
+			size_factor = static_cast<float>(tmpVal)/100.0f;
+		}
+	}
+}

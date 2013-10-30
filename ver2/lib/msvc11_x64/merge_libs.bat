fa51485d477fa0baedf15a45c3939c4956333f9e
Miroslav Andel <miroslav.andel@gmail.com>
2013-10-30 19:21:43 +0000

    Setting line endings to leave-as-it-is and adding merge bat files to all visual studio projects.

---
+++ ver2/lib/msvc11_x64/merge_libs.bat
@@ -0,0 +1,6 @@
+DEL sgct.lib
+DEL sgctd.lib
+call "%VS110COMNTOOLS%"\vsvars32.bat
+lib /LTCG /OUT:sgct.lib sgct_light.lib deps\freetype.lib deps\glew.lib deps\glfw3.lib deps\libpng15_static.lib deps\tinythreadpp.lib deps\tinyxml2.lib deps\vrpn.lib deps\zlibstatic.lib
+lib /LTCG /OUT:sgctd.lib sgct_lightd.lib deps\freetyped.lib deps\glewd.lib deps\glfw3d.lib deps\libpng15_staticd.lib deps\tinythreadppd.lib deps\tinyxml2d.lib deps\vrpnd.lib deps\zlibstaticd.lib
+pause

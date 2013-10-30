6b08217d8956253693a8433043c8b6b42244f371
Miroslav Andel <miroslav.andel@gmail.com>
2013-10-30 02:59:13 +0000

    Updated cmake files for better OSX compability

--- ver2/lib/mac/merge_libs.sh
+++ ver2/lib/mac/merge_libs.sh
@@ -2,7 +2,7 @@
 # merge libs
 echo "Merging release libs... "
 rm libsgct.a
-ar x deps/libGLEW.a
+ar x deps/libglew.a
 ar x deps/libglfw3.a
 ar x deps/libz.a
 ar x deps/libpng15.a
@@ -15,14 +15,14 @@ ar r libsgct.a *.o
 rm *.o
 echo "Merging debug libs... "
 rm libsgctd.a
-ar x deps/libGLEW.a
-ar x deps/libglfw3.a
-ar x deps/libz.a
-ar x deps/libpng15.a
-ar x deps/libfreetype.a
-ar x deps/libtinyxml2_d.a
-ar x deps/libtinythreadpp.a
-ar x deps/libvrpn.a
+ar x deps/libglewd.a
+ar x deps/libglfw3d.a
+ar x deps/libzd.a
+ar x deps/libpng15d.a
+ar x deps/libfreetyped.a
+ar x deps/libtinyxml2d.a
+ar x deps/libtinythreadppd.a
+ar x deps/libvrpnd.a
 ar x libsgct_lightd.a
 ar r libsgctd.a *.o
 rm *.o

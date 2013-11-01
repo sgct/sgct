cbcbff3947e92c8c89d97f775bae6dcabcea0178
Miroslav Andel <miroslav.andel@gmail.com>
2013-11-01 18:27:35 +0000

    Release version for OS X, SGCT 2.0.6

---
+++ ver2/framework/SGCT/SGCT/SGCT.h
@@ -0,0 +1,14 @@
+//
+//  SGCT.h
+//  SGCT
+//
+//  Created by Miroslav Andel on 10/31/13.
+//  Copyright (c) 2013 Linköping University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "sgct.h"
+
+@interface SGCT : NSObject
+
+@end
---
+++ ver2/framework/SGCT/SGCT/SGCT.m
@@ -0,0 +1,13 @@
+//
+//  SGCT.m
+//  SGCT
+//
+//  Created by Miroslav Andel on 10/31/13.
+//  Copyright (c) 2013 Linköping University. All rights reserved.
+//
+
+#import "SGCT.h"
+
+@implementation SGCT
+
+@end

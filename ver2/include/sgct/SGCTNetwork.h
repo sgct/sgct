fa096d337c8bb089384541c94166b29f64beb664
Miroslav Andel <miroslav.andel@gmail.com>
2013-11-01 02:25:16 +0000

    Updated visual studio projects and cmakes. Removed autobuild-files and libs.

--- ver2/include/sgct/SGCTNetwork.h
+++ ver2/include/sgct/SGCTNetwork.h
@@ -29,14 +29,17 @@ The macros below set the propper c++11 includes and namespaces
         namespace sgct_cppxeleven = std;
         //#pragma message "SGCTNetwork will use std::functional"
     #endif
+	#define __LOAD_CPP11_FUN__
 #elif _MSC_VER >= 1400 //visual studio 2005 or later
 	#include <functional>
     namespace sgct_cppxeleven = std::tr1;
     //#pragma message "SGCTNetwork will use std::tr1::functional"
+	#define __LOAD_CPP11_FUN__
 #elif __USE_CPP11__ || defined(__LINUX__)
     #include <tr1/functional>
     namespace sgct_cppxeleven = std::tr1;
     //#pragma message "SGCTNetwork will use std:tr1::functional"
+	#define __LOAD_CPP11_FUN__
 #endif
 
 #define MAX_NET_SYNC_FRAME_NUMBER 10000
@@ -68,7 +71,7 @@ public:
 	void init(const std::string port, const std::string address, bool _isServer, int id, ConnectionTypes serverType, bool firmSync);
 	void closeNetwork(bool forced);
 	void initShutdown();
-#if __USE_CPP11__ || defined(__LINUX__)
+#ifdef __LOAD_CPP11_FUN__
 	void setDecodeFunction(sgct_cppxeleven::function<void (const char*, int, int)> callback);
 	void setUpdateFunction(sgct_cppxeleven::function<void (int)> callback);
 	void setConnectedFunction(sgct_cppxeleven::function<void (void)> callback);
@@ -98,7 +101,7 @@ public:
 
 	SGCT_SOCKET mSocket;
 	SGCT_SOCKET mListenSocket;
-#if __USE_CPP11__ || defined(__LINUX__)
+#ifdef __LOAD_CPP11_FUN__
 	sgct_cppxeleven::function< void(const char*, int, int) > mDecoderCallbackFn;
 	sgct_cppxeleven::function< void(int) > mUpdateCallbackFn;
 	sgct_cppxeleven::function< void(void) > mConnectedCallbackFn;

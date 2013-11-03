24200eff1fea5052f875e63817286f95272e66ad
Miroslav Andel <miroslav.andel@gmail.com>
2013-11-03 01:27:13 +0000

    Resolved msvc x64 issue in zlib cmake. Corrected minor issues in Alut cmake.

--- ver2/lib/mingw/merge.bat
+++ ver2/lib/mingw/merge.bat
@@ -3,11 +3,11 @@ ar x deps/libglew.a
 ar x deps/libglfw3.a
 ar x deps/libzlibstatic.a
 ar x deps/libpng15.a
-ar x deps/freetype.a
+ar x deps/libfreetype.a
 ar x deps/libtinyxml2.a
 ar x deps/libtinythreadpp.a
 ar x deps/libvrpn.a
 ar x libsgct_light.a
-ar r libsgct.a *.o *.obj
+ar r libsgct.a *.obj
 DEL *.o *.obj
 pause

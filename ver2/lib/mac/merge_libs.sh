823004cf5eaf87ad2334de1320985df74e941042
Miroslav Andel <miroslav.andel@gmail.com>
2013-11-13 00:39:22 +0000

    Updated CMakes for enabling sgct install.

--- ver2/lib/mac/merge_libs.sh
+++ ver2/lib/mac/merge_libs.sh
@@ -1,29 +1,64 @@
 #!/bin/bash
 # merge libs
-echo "Merging release libs... "
-rm libsgct.a
-ar x deps/libglew.a
-ar x deps/libglfw3.a
-ar x deps/libz.a
-ar x deps/libpng15.a
-ar x deps/libfreetype.a
-ar x deps/libtinyxml2.a
-ar x deps/libtinythreadpp.a
-ar x deps/libvrpn.a
-ar x libsgct_light.a
-ar r libsgct.a *.o
-rm *.o
-echo "Merging debug libs... "
-rm libsgctd.a
-ar x deps/libglewd.a
-ar x deps/libglfw3d.a
-ar x deps/libzd.a
-ar x deps/libpng15d.a
-ar x deps/libfreetyped.a
-ar x deps/libtinyxml2d.a
-ar x deps/libtinythreadppd.a
-ar x deps/libvrpnd.a
-ar x libsgct_lightd.a
-ar r libsgctd.a *.o
-rm *.o
-echo "Done."
+if [ -f libsgct_light.a ] && [ -f libsgct_lightd.a ];
+then
+	echo "Merging release libs... "
+	rm libsgct.a
+	ar x deps/libglew.a
+	ar x deps/libglfw3.a
+	ar x deps/libz.a
+	ar x deps/libpng15.a
+	ar x deps/libfreetype.a
+	ar x deps/libtinyxml2.a
+	ar x deps/libtinythreadpp.a
+	ar x deps/libvrpn.a
+	ar x libsgct_light.a
+	ar r libsgct.a *.o
+	rm *.o
+	echo "Merging debug libs... "
+	rm libsgctd.a
+	ar x deps/libglewd.a
+	ar x deps/libglfw3d.a
+	ar x deps/libzd.a
+	ar x deps/libpng15d.a
+	ar x deps/libfreetyped.a
+	ar x deps/libtinyxml2d.a
+	ar x deps/libtinythreadppd.a
+	ar x deps/libvrpnd.a
+	ar x libsgct_lightd.a
+	ar r libsgctd.a *.o
+	rm *.o
+	echo "Done."
+elif [ -f libsgct_light.a ];
+then
+	echo "Merging release libs... "
+	rm libsgct.a
+	ar x deps/libglew.a
+	ar x deps/libglfw3.a
+	ar x deps/libz.a
+	ar x deps/libpng15.a
+	ar x deps/libfreetype.a
+	ar x deps/libtinyxml2.a
+	ar x deps/libtinythreadpp.a
+	ar x deps/libvrpn.a
+	ar x libsgct_light.a
+	ar r libsgct.a *.o
+	rm *.o
+	echo "Done."
+elif [ -f libsgct_lightd.a ];
+then
+	echo "Merging debug libs... "
+	rm libsgctd.a
+	ar x deps/libglewd.a
+	ar x deps/libglfw3d.a
+	ar x deps/libzd.a
+	ar x deps/libpng15d.a
+	ar x deps/libfreetyped.a
+	ar x deps/libtinyxml2d.a
+	ar x deps/libtinythreadppd.a
+	ar x deps/libvrpnd.a
+	ar x libsgct_lightd.a
+	ar r libsgctd.a *.o
+	rm *.o
+	echo "Done."
+fi
--- ver2/lib/mac/merge_libs_xcode.sh
+++ ver2/lib/mac/merge_libs_xcode.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 # merge libs
-echo "Merging release libs... "
-rm libsgct.a
-libtool -static -o libsgct.a deps/libglew.a deps/libglfw3.a deps/libz.a deps/libpng15.a deps/libfreetype.a deps/libtinyxml2.a deps/libtinythreadpp.a deps/libvrpn.a libsgct_light.a
-echo "Merging debug libs... "
-rm libsgctd.a
-libtool -static -o libsgctd.a deps/libglewd.a deps/libglfw3d.a deps/libzd.a deps/libpng15d.a deps/libfreetyped.a deps/libtinyxml2d.a deps/libtinythreadppd.a deps/libvrpnd.a libsgct_lightd.a
-echo "Done."
+if [ -f libsgct_light.a ] && [ -f libsgct_lightd.a ];
+then
+	echo "Merging release libs... "
+	rm libsgct.a
+	libtool -static -o libsgct.a deps/libglew.a deps/libglfw3.a deps/libz.a deps/libpng15.a deps/libfreetype.a deps/libtinyxml2.a deps/libtinythreadpp.a deps/libvrpn.a libsgct_light.a
+	echo "Merging debug libs... "
+	rm libsgctd.a
+	libtool -static -o libsgctd.a deps/libglewd.a deps/libglfw3d.a deps/libzd.a deps/libpng15d.a deps/libfreetyped.a deps/libtinyxml2d.a deps/libtinythreadppd.a deps/libvrpnd.a libsgct_lightd.a
+	echo "Done."
+elif [ -f libsgct_light.a ];
+then
+	echo "Merging release libs... "
+	rm libsgct.a
+	libtool -static -o libsgct.a deps/libglew.a deps/libglfw3.a deps/libz.a deps/libpng15.a deps/libfreetype.a deps/libtinyxml2.a deps/libtinythreadpp.a deps/libvrpn.a libsgct_light.a
+	echo "Done."
+elif [ -f libsgct_lightd.a ];
+then
+	echo "Merging debug libs... "
+	rm libsgctd.a
+	libtool -static -o libsgctd.a deps/libglewd.a deps/libglfw3d.a deps/libzd.a deps/libpng15d.a deps/libfreetyped.a deps/libtinyxml2d.a deps/libtinythreadppd.a deps/libvrpnd.a libsgct_lightd.a
+	echo "Done."
+fi

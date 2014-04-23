d4220cd31c5b62ec79ce2f2edec4b33a213e7fa7
Miroslav Andel <miroslav.andel@gmail.com>
2014-04-23 15:34:29 +0000

    Updated doxygen documentation for main page.

--- include/sgct.h
+++ include/sgct.h
@@ -124,7 +124,7 @@ SGCT applications are scalable and use a XML configuration file where all cluste
 SGCT does also support running cluster applications on a single computer for testing purposes.
 
 \subsection licence Licence
-Copyright (c) 2012-2014 Miroslav Andel\n
+Copyright (c) 2012-2014 Miroslav Andel, Link&ouml;ping University\n
 All rights reserved.\n
 \n
 Contributors: Alexander Fridlund, Alexander Bock, Joel Kronander, Daniel J&ouml;nsson\n
@@ -139,9 +139,9 @@ are permitted provided that the following conditions are met:
 	notice, this list of conditions and the following disclaimer in the\n
 	documentation and/or other materials provided with the distribution.
 
-    -# Neither the name of the Link&ouml;ping University nor the\n
-	names of its contributors may be used to endorse or promote products\n
-	derived from this software without specific prior written permission.
+    -# Neither the name of the copyright holder nor the names of its contributors
+    may be used to endorse or promote products derived from this software
+    without specific prior written permission.
 
 \n
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS''\n
@@ -154,7 +154,7 @@ OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABIL
 WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)\n
 ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n
 
-\subsection dependencies Dependencies
+\subsection dependencies Third party libraries
 SGCT is using the following libraries.
 - <a href="http://www.glfw.org">GLFW 3</a>
 - <a href="http://glew.sourceforge.net/">GLEW - The OpenGL Extension Wrangler Library</a>
@@ -162,10 +162,260 @@ SGCT is using the following libraries.
 - <a href="http://tinythreadpp.bitsnbites.eu/">TinyThread++</a>
 - <a href="http://www.grinninglizard.com/tinyxml2/index.html">TinyXML-2</a>
 - <a href="http://www.cs.unc.edu/Research/vrpn/">VRPN - Virtual Reality Peripheral Network</a>
-- <a href="http://www.freetype.org">Freetype</a>
-- <a href="http://www.zlib.net/">ZLib</a>
-- <a href="http://www.libpng.org/">libPNG</a>
+- <a href="http://www.freetype.org">Freetype 2</a>
+- <a href="http://www.zlib.net/">zlib</a>
+- <a href="http://www.libpng.org/">libpng</a>
+- <a href="http://www.libjpeg-turbo.org/">libjpeg-turbo</a>
 
+\subsection copyrights Third party copyright notes
+
+<SMALL>
+\subsubsection glfw GLFW
+Copyright &copy; 2002-2006 Marcus Geelnard\n
+Copyright &copy; 2006-2011 Camilla Berglund\n
+\n
+This software is provided ‘as-is’, without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.\n
+Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:\n
+
+   -# The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+   -# Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+   -# This notice may not be removed or altered from any source distribution.
+
+License type: zlib/libpng\n
+
+\subsubsection glew GLEW - The OpenGL Extension Wrangler Library
+The OpenGL Extension Wrangler Library\n
+Copyright &copy; 2008-2013, Nigel Stewart <nigels[]users sourceforge net>\n
+Copyright &copy; 2002-2008, Milan Ikits <milan ikits[]ieee org>\n
+Copyright &copy; 2002-2008, Marcelo E. Magallon <mmagallo[]debian org>\n
+Copyright &copy; 2002, Lev Povalahev\n
+All rights reserved.\n
+\n
+Redistribution and use in source and binary forms, with or without\n
+modification, are permitted provided that the following conditions are met:\n
+
+   - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+   - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+   - The name of the author may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+\n
+\n
+License type: MIT
+
+\subsubsection glm GLM - OpenGL Mathematics
+Copyright &copy; 2005 - 2013 G-Truc Creation (www.g-truc.net)\n
+License: <a href="http://glm.g-truc.net/copying.txt">http://glm.g-truc.net/copying.txt</a>\n
+License type: MIT\n
+
+\subsubsection tinythread TinyThread++
+Copyright &copy; 2010-2012 Marcus Geelnard\n
+\n
+This software is provided 'as-is', without any express or implied\n
+warranty. In no event will the authors be held liable for any damages\n
+arising from the use of this software.\n
+\n
+Permission is granted to anyone to use this software for any purpose,\n
+including commercial applications, and to alter it and redistribute it\n
+freely, subject to the following restrictions:\n
+\n
+   -# The origin of this software must not be misrepresented; you must not\n
+   claim that you wrote the original software. If you use this software\n
+   in a product, an acknowledgment in the product documentation would be\n
+   appreciated but is not required.
+
+   -# Altered source versions must be plainly marked as such, and must not be\n
+   misrepresented as being the original software.
+
+   -# This notice may not be removed or altered from any source distribution.
+
+
+License type: zlib/libpng
+
+\subsubsection tinyxml TinyXML-2
+Original code by Lee Thomason (www.grinninglizard.com)\n
+\n
+This software is provided 'as-is', without any express or implied\n
+warranty. In no event will the authors be held liable for any\n
+damages arising from the use of this software.\n
+\n
+Permission is granted to anyone to use this software for any\n
+purpose, including commercial applications, and to alter it and\n
+redistribute it freely, subject to the following restrictions:\n
+\n
+   -# The origin of this software must not be misrepresented; you must\n
+   not claim that you wrote the original software. If you use this\n
+   software in a product, an acknowledgment in the product documentation\n
+   would be appreciated but is not required.
+
+   -# Altered source versions must be plainly marked as such, and\n
+   must not be misrepresented as being the original software.
+
+   -# This notice may not be removed or altered from any source distribution.
+
+
+License type: zlib
+
+\subsubsection vrpn VRPN - Virtual Reality Peripheral Network
+The Virtual Reality Peripheral Network (VRPN) is public-domain software\n
+released by the Department of Computer Science at\n
+the University of North Carolina at Chapel Hill\n
+Acknowledgement: The CISMM project at the University of North Carolina at Chapel Hill, supported by NIH/NCRR and NIH/NIBIB award #2P41EB002025\n
+\n
+License type: Boost Software License 1.0\n
+
+\subsubsection freetype Freetype 2
+Copyright &copy; 2011 The FreeType Project (www.freetype.org). All rights reserved.\n
+Copyright &copy; 1996-2002, 2006 by David Turner, Robert Wilhelm, and Werner Lemberg\n
+\n
+License: <a href="http://www.freetype.org/license.html">http://www.freetype.org/license.html</a>\n
+License type: BSD\n
+
+\subsubsection zlib zlib
+&copy; 1995-2012 Jean-loup Gailly and Mark Adler\n
+\n
+This software is provided 'as-is', without any express or implied\n
+warranty.  In no event will the authors be held liable for any damages\n
+arising from the use of this software.\n
+\n
+Permission is granted to anyone to use this software for any purpose,\n
+including commercial applications, and to alter it and redistribute it\n
+freely, subject to the following restrictions:\n
+
+   -# The origin of this software must not be misrepresented; you must not\n
+   claim that you wrote the original software. If you use this software\n
+   in a product, an acknowledgment in the product documentation would be\n
+   appreciated but is not required.
+   -# Altered source versions must be plainly marked as such, and must not be\n
+   misrepresented as being the original software.
+   -# This notice may not be removed or altered from any source distribution.
+
+Jean-loup Gailly jloup@gzip.org\n
+Mark Adler madler@alumni.caltech.edu\n
+\n
+License type: zlib
+
+\subsubsection libpng libpng - Portable Network Graphics library
+This copy of the libpng notices is provided for your convenience.  In case of\n
+any discrepancy between this copy and the notices in the file png.h that is\n
+included in the libpng distribution, the latter shall prevail.\n
+\n
+COPYRIGHT NOTICE, DISCLAIMER, and LICENSE:\n
+\n
+If you modify libpng you may insert additional notices immediately following\n
+this sentence.\n
+\n
+This code is released under the libpng license.\n
+\n
+libpng versions 1.2.6, August 15, 2004, through 1.5.14, January 24, 2013, are\n
+Copyright &copy; 2004, 2006-2012 Glenn Randers-Pehrson, and are\n
+distributed according to the same disclaimer and license as libpng-1.2.5\n
+with the following individual added to the list of Contributing Authors\n
+
+   - Cosmin Truta
+
+libpng versions 1.0.7, July 1, 2000, through 1.2.5 - October 3, 2002, are\n
+Copyright &copy; 2000-2002 Glenn Randers-Pehrson, and are\n
+distributed according to the same disclaimer and license as libpng-1.0.6\n
+with the following individuals added to the list of Contributing Authors\n
+
+   - Simon-Pierre Cadieux
+   - Eric S. Raymond
+   - Gilles Vollant
+
+and with the following additions to the disclaimer:\n
+\n
+   There is no warranty against interference with your enjoyment of the\n
+   library or against infringement.  There is no warranty that our\n
+   efforts or the library will fulfill any of your particular purposes\n
+   or needs.  This library is provided with all faults, and the entire\n
+   risk of satisfactory quality, performance, accuracy, and effort is with\n
+   the user.\n
+   \n
+libpng versions 0.97, January 1998, through 1.0.6, March 20, 2000, are\n
+Copyright (c) 1998, 1999 Glenn Randers-Pehrson, and are\n
+distributed according to the same disclaimer and license as libpng-0.96,\n
+with the following individuals added to the list of Contributing Authors:\n
+
+   - Tom Lane
+   - Glenn Randers-Pehrson
+   - Willem van Schaik
+
+libpng versions 0.89, June 1996, through 0.96, May 1997, are\n
+Copyright &copy; 1996, 1997 Andreas Dilger\n
+Distributed according to the same disclaimer and license as libpng-0.88,\n
+with the following individuals added to the list of Contributing Authors:\n
+
+   - John Bowler
+   - Kevin Bracey
+   - Sam Bushell
+   - Magnus Holmgren
+   - Greg Roelofs
+   - Tom Tanner
+
+libpng versions 0.5, May 1995, through 0.88, January 1996, are\n
+Copyright &copy; 1995, 1996 Guy Eric Schalnat, Group 42, Inc.\n
+\n
+For the purposes of this copyright and license, "Contributing Authors"\n
+is defined as the following set of individuals:\n
+
+   - Andreas Dilger
+   - Dave Martindale
+   - Guy Eric Schalnat
+   - Paul Schmidt
+   - Tim Wegner
+
+The PNG Reference Library is supplied "AS IS".  The Contributing Authors\n
+and Group 42, Inc. disclaim all warranties, expressed or implied,\n
+including, without limitation, the warranties of merchantability and of\n
+fitness for any purpose.  The Contributing Authors and Group 42, Inc.\n
+assume no liability for direct, indirect, incidental, special, exemplary,\n
+or consequential damages, which may result from the use of the PNG\n
+Reference Library, even if advised of the possibility of such damage.\n
+\n
+Permission is hereby granted to use, copy, modify, and distribute this\n
+source code, or portions hereof, for any purpose, without fee, subject\n
+to the following restrictions:\n
+\n
+   -# The origin of this source code must not be misrepresented.
+   -# Altered versions must be plainly marked as such and must not\n
+   be misrepresented as being the original source.
+
+   -# This Copyright notice may not be removed or altered from any\n
+   source or altered source distribution.
+
+The Contributing Authors and Group 42, Inc. specifically permit, without\n
+fee, and encourage the use of this source code as a component to\n
+supporting the PNG file format in commercial products.  If you use this\n
+source code in a product, acknowledgment is not required but would be\n
+appreciated.\n
+\n
+A "png_get_copyright" function is available, for convenient use in "about"\n
+boxes and the like:\n
+\n
+   printf("%s",png_get_copyright(NULL));\n
+\n
+Also, the PNG logo (in PNG format, of course) is supplied in the\n
+files "pngbar.png" and "pngbar.jpg (88x31) and "pngnow.png" (98x31).\n
+\n
+Libpng is OSI Certified Open Source Software.  OSI Certified Open Source is a\n
+certification mark of the Open Source Initiative.\n
+\n
+Glenn Randers-Pehrson\n
+glennrp at users.sourceforge.net\n
+January 24, 2013\n
+\n
+License: <a href="http://www.libpng.org/pub/png/src/libpng-LICENSE.txt">http://www.libpng.org/pub/png/src/libpng-LICENSE.txt</a>\n
+License type: libpng
+
+\subsubsection libjpegturbo libjpeg-turbo
+Copyright &copy; 1991-2012, Thomas G. Lane, Guido Vollbeding\n
+Modified 2002-2009 by Guido Vollbeding.\n
+Copyright &copy; 2009-2011, 2013, D. R. Commander.\n
+Acknowledgement: "this software is based in part on the work of the Independent JPEG Group"\n
+\n
+License type: BSD\n
+</SMALL>
 \n
 */
 

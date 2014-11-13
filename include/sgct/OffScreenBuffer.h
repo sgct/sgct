038739553483f2ee1d8328ac797c1efe854159c2
Miroslav Andel <miroslav.andel@gmail.com>
2014-11-13 09:05:02 +0000

    Modified offscreenbuffer's include path.

--- include/sgct/OffScreenBuffer.h
+++ include/sgct/OffScreenBuffer.h
@@ -8,7 +8,7 @@ For conditions of distribution and use, see copyright notice in sgct.h
 #ifndef _OFF_SCREEN_BUFFER
 #define _OFF_SCREEN_BUFFER
 
-#include "../include/sgct/ogl_headers.h"
+#include "ogl_headers.h"
 
 namespace sgct_core
 {

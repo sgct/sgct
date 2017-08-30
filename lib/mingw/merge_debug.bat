de0ac23db8a27e4e2d5b426e5203f23e723daf84
Alexander Bock <alexander.bock@me.com>
2017-08-30 11:26:16 -0400

    Remove TinyThread library references

--- lib/mingw/merge_debug.bat
+++ lib/mingw/merge_debug.bat
@@ -6,7 +6,6 @@ ar x deps/libpng16d.a
 ar x deps/libturbojpegd.a
 ar x deps/libfreetyped.a
 ar x deps/libtinyxml2d.a
-ar x deps/libtinythreadppd.a
 ar x deps/libvrpnd.a
 ar x libsgct_lightd.a
 ar r libsgctd.a *.obj

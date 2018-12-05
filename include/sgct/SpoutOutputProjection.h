51e3a97339498ac851e120016718a12f09a5d5eb
Marco Silva <marco@multimeios.pt>
2018-12-05 19:29:52 +0000

    added support for multi channel select in spout output projection

--- include/sgct/SpoutOutputProjection.h
+++ include/sgct/SpoutOutputProjection.h
@@ -13,60 +13,68 @@ For conditions of distribution and use, see copyright notice in sgct.h
 
 namespace sgct_core
 {
-	/*!
-	This class manages and renders non linear fisheye projections
-	*/
-	class SpoutOutputProjection : public NonLinearProjection
-	{
-	public:
-		SpoutOutputProjection();
-		~SpoutOutputProjection();
+    /*!
+    This class manages and renders non linear fisheye projections
+    */
+    class SpoutOutputProjection : public NonLinearProjection
+    {
+    public:
+        SpoutOutputProjection();
+        ~SpoutOutputProjection();
 
-		void update(float width, float height);
-		void render();
-		void renderCubemap(std::size_t * subViewPortIndex);
+        enum Mapping {
+            Fisheye,
+            Equirectangular,
+            Cubemap
+        };
 
-	private:
-		void initTextures();
-		void initViewports();
-		void initShaders();
+        void setSpoutChannels(bool channels[6]);
+        void setSpoutMappingName(std::string name);
+        void setSpoutMapping(Mapping type);
+        void setSpoutRigOrientation(glm::vec3 orientation);
+        void update(float width, float height);
+        void render();
+        void renderCubemap(std::size_t * subViewPortIndex);
 
-		void drawCubeFace(const std::size_t & face);
-		void blitCubeFace(const int & face);
-		void attachTextures(const int & face);
-		void renderInternal();
-		void renderInternalFixedPipeline();
-		void renderCubemapInternal(std::size_t * subViewPortIndex);
-		void renderCubemapInternalFixedPipeline(std::size_t * subViewPortIndex);
+        static const size_t spoutTotalFaces = 6;
+        static const std::string spoutCubeMapFaceName[spoutTotalFaces];
 
-		void(SpoutOutputProjection::*mInternalRenderFn)(void);
-		void(SpoutOutputProjection::*mInternalRenderCubemapFn)(std::size_t *);
+    private:
+        void initTextures();
+        void initViewports();
+        void initShaders();
+        void initFBO();
+        void updateGeomerty(const float & width, const float & height);
 
-		//shader locations
-		int mSwapColorLoc, mSwapDepthLoc, mSwapNearLoc, mSwapFarLoc;
-		/*
-		// for 4 faces
-		const std::string spoutCubeMapFaceName[6] = {
-			"Right",
-			"Bottom",
-			"Top",
-			"Left",
-			"zLeft",
-			"zRight",
-		};
-		*/
-		// for 6 faces
-		const std::string spoutCubeMapFaceName[6] = {
-			"Right",
-			"zLeft",
-			"Bottom",
-			"Top",
-			"Left",
-			"zRight",
-		};
-		void *handle[6];
-		GLuint spoutTexture[6];
-	};
+        void drawCubeFace(const std::size_t & face);
+        void blitCubeFace(const int & face);
+        void attachTextures(const int & face);
+        void renderInternal();
+        void renderInternalFixedPipeline();
+        void renderCubemapInternal(std::size_t * subViewPortIndex);
+        void renderCubemapInternalFixedPipeline(std::size_t * subViewPortIndex);
+
+        void(SpoutOutputProjection::*mInternalRenderFn)(void);
+        void(SpoutOutputProjection::*mInternalRenderCubemapFn)(std::size_t *);
+
+        //shader locations
+        int mCubemapLoc, mHalfFovLoc, mSwapColorLoc, mSwapDepthLoc, mSwapNearLoc, mSwapFarLoc;
+
+        OffScreenBuffer * mSpoutFBO_Ptr;
+
+        void *handle[spoutTotalFaces];
+        GLuint spoutTexture[spoutTotalFaces];
+        bool spoutEnabled[spoutTotalFaces];
+
+        void *spoutMappingHandle;
+        GLuint spoutMappingTexture;
+        Mapping spoutMappingType;
+        std::string spoutMappingName;
+        glm::vec3 spoutRigOrientation;
+
+        int spoutMappingWidth;
+        int spoutMappingHeight;
+    };
 
 }
 

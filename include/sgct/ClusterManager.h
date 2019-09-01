0b80ef9fc7eae58e03836323dbdbb7a9fadd9c89
Alexander Bock <mail@alexanderbock.eu>
2019-09-01 16:53:40 +0200

    Started cleanup of ClusterManager

--- include/sgct/ClusterManager.h
+++ include/sgct/ClusterManager.h
@@ -16,118 +16,176 @@ For conditions of distribution and use, see copyright notice in sgct.h
 #include <string>
 
 
-/*! \namespace sgct_core
-\brief SGCT core namespace is used internally within sgct.
-*/
+/**
+ * \namespace sgct_core
+ * \brief SGCT core namespace is used internally within sgct.
+ */
 namespace sgct_core {
 
 class SGCTUser;
 
-/*!
-The ClusterManager manages all nodes and cluster settings.
-This class is a static singleton and is accessed using it's instance.
-*/
+/**
+ * The ClusterManager manages all nodes and cluster settings. This class is a static
+ * singleton and is accessed using it's instance.
+ */
 class ClusterManager {
 public:
-    /*! Get the ClusterManager instance */
+    /// Get the ClusterManager instance */
     static ClusterManager* instance();
 
-    /*! Destroy the ClusterManager */
+    /// Destroy the ClusterManager */
     static void destroy();
 
-    /*!
-        Different modes for warping/edge blending meshes 
-    */
+    /// Different modes for warping/edge blending meshes 
     enum class MeshImplementation {
         BufferObjects = 0,
         DisplayList
     };
 
+    /// Add a cluster node to the manager's vector.
     void addNode(SGCTNode node);
+
+    /// Add a user ptr.
     void addUser(std::unique_ptr<SGCTUser> userPtr);
 
+    /**
+     * Get a pointer to a specific node.
+     *
+     * \param index the index to a node in the vector
+     *
+     * \return the pointer to the requested node or nullptr if not found
+     */
     SGCTNode* getNodePtr(size_t index);
+
+    /**
+     * Get a pointer to a specific node.
+     *
+     * \param name of the node to search for
+     *
+     * \return the pointer to the requested node or NULL if not found
+     */
     SGCTNode* getNodePtr(const std::string& name);
+
+    /// \return a pointer to the node that this application is running on
     SGCTNode* getThisNode();
+
+    /// \return the pointer to the default user
     SGCTUser* getDefaultUser();
+
+    /// \return the pointer to a named user. nullptr is returned if no user is found.
     SGCTUser* getUserPtr(const std::string& name);
+
+    /// \return the pointer to the tracked user. Returns nullptr if no user is tracked.
     SGCTUser* getTrackedUserPtr();
+
+    /// \return the current network mode
     NetworkManager::NetworkMode getNetworkMode() const;
+
+    /// Sets the current network mode
     void setNetworkMode(NetworkManager::NetworkMode nm);
 
-    /*!
-        \returns the number of nodes in the cluster
-    */
+    /// \return the number of nodes in the cluster
     size_t getNumberOfNodes() const;
     
-    /*!
-        \returns the scene transform specified in the configuration file
-    */
+    /// \return the scene transform specified in the configuration file
     const glm::mat4& getSceneTransform() const;
 
-    /*!
-        Don't set this, this is done automatically using from the Network Manager which compares
-        ip addresses from this computer to the XML config file.
-        
-        \param the index to the node where this application is running on
-    */
+    /**
+     *  Don't set this, this is done automatically using from the Network Manager which
+     * compares IP addresses from this computer to the XML config file.
+     *
+     * \param the index to the node where this application is running on
+     */
     void setThisNodeId(int id);
 
-    /*!
-        \returns the id to the node which runs this application
-    */
+    /// \returns the id to the node which runs this application
     int getThisNodeId() const;
 
+    /**
+     * \returns the dns, name or IP of the master in the cluster (depends on what's been
+     * set in the XML config)
+     */
     const std::string& getMasterAddress() const;
+
+    /// \param the DNS, IP or name of the master in the cluster
     void setMasterAddress(std::string address);
 
-    /*!
-        \returns state of the firm frame lock lock sync
-    */
+    /// \return state of the firm frame lock lock sync
     bool getFirmFrameLockSyncStatus() const;
 
-    /*!
-        \param the state of the firm frame lock sync
-    */
+    /// \param the state of the firm frame lock sync
     void setFirmFrameLockSyncStatus(bool state);
 
+    /**
+     * \return the external control port number if it's set or specified in the XML
+     * configuration
+     */
     const std::string& getExternalControlPort() const;
+
+    /// \param the external control port number
     void setExternalControlPort(std::string port);
 
+    /// Set if external control should use ASCII (Telnet) or raw binary parsing.
     void setUseASCIIForExternalControl(bool useASCII);
+
+    /// Get if external control is using ASCII (Telnet) or raw binary parsing.
     bool getUseASCIIForExternalControl() const;
 
+    /// Set if software sync between nodes should be ignored
     void setUseIgnoreSync(bool state);
+
+    /// Get if software sync between nodes is disabled
     bool getIgnoreSync() const;
 
+    /// Set the scene transform.
     void setSceneTransform(glm::mat4 mat);
+
+    /**
+     * Set the scene offset/translation. This is set using the XML config file for easier
+     * transitions between different hardware setups.
+     */
     void setSceneOffset(glm::vec3 offset);
+
+    /**
+     * Set the scene rotation. This is set using the XML config file for easier
+     * transitions between different hardware setups.
+     */
     void setSceneRotation(float yaw, float pitch, float roll);
+
+    /**
+     * Set the scene rotation. This is set using the XML config file for easier
+     * transitions between different hardware setups.
+     */
     void setSceneRotation(glm::mat4 mat);
+    
+    /**
+     * Set the scene scale. This is set using the XML config file for easier transitions
+     * between different hardware setups.
+     */
     void setSceneScale(float scale);
 
-    /*!
-        Sets the rendering hint for SGCT's internal draw functions (must be done before opengl init)
-    */
+    /**
+     * Sets the rendering hint for SGCT's internal draw functions (must be done before
+     * OpenGL init)
+     */
     void setMeshImplementation(MeshImplementation impl);
 
-    /*!
-        \returns the mesh implementation
-    */
+    /// \returns the mesh implementation
     MeshImplementation getMeshImplementation() const;
 
-    /*!
-        \returns the pointer to the tracking manager
-    */
+    /// \returns the pointer to the tracking manager
     sgct::SGCTTrackingManager& getTrackingManager();
 
 private:
     ClusterManager();
     ~ClusterManager() = default;
 
+    /**
+     * Updates the scene transform. Calculates the transform matrix using:
+     * SceneTransform = Rotation * Offset * Scale
+     */
     void calculateSceneTransform();
 
-private:
     static ClusterManager* mInstance;
 
     std::vector<SGCTNode> nodes;
--- src/sgct/ClusterManager.cpp
+++ src/sgct/ClusterManager.cpp
@@ -27,46 +27,26 @@ ClusterManager* ClusterManager::instance()  {
 }
 
 void ClusterManager::destroy() {
-    if (mInstance) {
-        delete mInstance;
-        mInstance = nullptr;
-    }
+    delete mInstance;
+    mInstance = nullptr;
 }
 
 ClusterManager::ClusterManager() {
     mUsers.push_back(std::make_unique<SGCTUser>("default"));
 }
 
-/*!
-    Add a cluster node to the manager's vector.
-*/
 void ClusterManager::addNode(SGCTNode node) {
     nodes.push_back(std::move(node));
 }
 
-/*!
-    Add an user ptr. The cluster manager will deallocate the user upon destruction.
-*/
 void ClusterManager::addUser(std::unique_ptr<SGCTUser> userPtr) {
     mUsers.push_back(std::move(userPtr));
 }
 
-/*!
-    Get a pointer to a specific node.
-
-    \param index the index to a node in the vector
-    \returns the pointer to the requested node or NULL if not found
-*/
 SGCTNode* ClusterManager::getNodePtr(size_t index) {
     return (index < nodes.size()) ? &nodes[index] : nullptr;
 }
 
-/*!
-    Get a pointer to a specific node.
-
-    \param name of the node to search for
-    \returns the pointer to the requested node or NULL if not found
-*/
 SGCTNode* ClusterManager::getNodePtr(const std::string& name) {
     auto it = std::find_if(
         nodes.begin(),
@@ -81,23 +61,14 @@ SGCTNode* ClusterManager::getNodePtr(const std::string& name) {
     }
 }
 
-/*!
-    \returns a pointer to the node that this application is running on
-*/
 SGCTNode* ClusterManager::getThisNode() {
     return mThisNodeId < 0 ? nullptr : &nodes[mThisNodeId];
 }
 
-/*!
-\returns the pointer to the default user
-*/
 SGCTUser* ClusterManager::getDefaultUser() {
     return mUsers[0].get();
 }
 
-/*!
-\returns the pointer to a named user. NULL is returned if no user is found.
-*/
 SGCTUser* ClusterManager::getUserPtr(const std::string& name) {
     auto it = std::find_if(
         mUsers.begin(),
@@ -112,9 +83,6 @@ SGCTUser* ClusterManager::getUserPtr(const std::string& name) {
     }
 }
 
-/*!
-\returns the pointer to the tracked user. NULL is returned if no user is tracked.
-*/
 SGCTUser* ClusterManager::getTrackedUserPtr() {
     auto it = std::find_if(
         mUsers.begin(),
@@ -129,119 +97,71 @@ SGCTUser* ClusterManager::getTrackedUserPtr() {
     }
 }
 
-/*!
-\returns the current network mode
-*/
 NetworkManager::NetworkMode ClusterManager::getNetworkMode() const {
     return mNetMode;
 }
 
-/*!
-Sets the current network mode
-*/
 void ClusterManager::setNetworkMode(NetworkManager::NetworkMode nm) {
     mNetMode = nm;
 }
 
-/*!
-Set the scene transform.
-*/
 void ClusterManager::setSceneTransform(glm::mat4 mat) {
     mSceneTransform = std::move(mat);
 }
 
-/*!
-    Set the scene offset/translation. This is set using the XML config file for easier transitions between different hardware setups.
-*/
 void ClusterManager::setSceneOffset(glm::vec3 offset) {
     mSceneTranslate = glm::translate(glm::mat4(1.f), std::move(offset));
     calculateSceneTransform();
 }
 
-/*!
-    Set the scene rotation. This is set using the XML config file for easier transitions between different hardware setups.
-*/
 void ClusterManager::setSceneRotation(float yaw, float pitch, float roll) {
     mSceneRotation = glm::yawPitchRoll(yaw, pitch, roll);
     calculateSceneTransform();
 }
 
-/*!
-Set the scene rotation. This is set using the XML config file for easier transitions between different hardware setups.
-*/
 void ClusterManager::setSceneRotation(glm::mat4 mat) {
     mSceneRotation = std::move(mat);
     calculateSceneTransform();
 }
 
-/*!
-Get if software sync between nodes is disabled
-*/
 bool ClusterManager::getIgnoreSync() const {
     return mIgnoreSync;
 }
 
-/*!
-Set if software sync between nodes should be ignored
-*/
 void ClusterManager::setUseIgnoreSync(bool state) {
     mIgnoreSync = state;
 }
 
-/*!
-    Set if external control should use ASCII (Telnet) or raw binary parsing.
-*/
 void ClusterManager::setUseASCIIForExternalControl(bool useASCII) {
     mUseASCIIForExternalControl = useASCII;
 }
 
-/*!
-    Get if external control is using ASCII (Telnet) or raw binary parsing.
-*/
 bool ClusterManager::getUseASCIIForExternalControl() const {
     return mUseASCIIForExternalControl;
 }
 
-/*!
-    Set the scene scale. This is set using the XML config file for easier transitions between different hardware setups.
-*/
 void ClusterManager::setSceneScale(float scale) {
     mSceneScale = glm::scale(glm::mat4(1.f), glm::vec3(scale));
     calculateSceneTransform();
 }
 
-/*!
-Updates the scene transform. Calculates the transform matrix using: SceneTransform = Rotation * Offset * Scale
-*/
 void ClusterManager::calculateSceneTransform() {
     mSceneTransform = mSceneRotation * mSceneTranslate * mSceneScale;
 }
 
-/*!
-        \returns the dns, name or ip of the master in the cluster (depends on what's been set in the XML config)
-*/
 const std::string& ClusterManager::getMasterAddress() const {
     return mMasterAddress;
 }
 
-/*!
-    \param the dns, ip or name of the master in the cluster
-*/
 void ClusterManager::setMasterAddress(std::string address) {
     std::transform(address.begin(), address.end(), address.begin(), ::tolower);
     mMasterAddress = std::move(address);
 }
 
-/*!
-\returns the external control port number if it's set or specified in the XML configuration
-*/
 const std::string& ClusterManager::getExternalControlPort() const {
     return mExternalControlPort;
 }
 
-/*!
-\param the external control port number
-*/
 void ClusterManager::setExternalControlPort(std::string port) {
     mExternalControlPort = std::move(port);
 }

bf4a9ecf9d3d0780eb74075f4eb49dcc6b96faaa
Alexander Bock <mail@alexanderbock.eu>
2019-10-12 14:16:33 +0200

    Add new example for taking a screenshot from spout

---
+++ LICENSE.md
@@ -0,0 +1,102 @@
+SGCT
+Simple Graphics Cluster Toolkit
+
+Copyright (c) 2012-2019
+Miroslav Andel, Linköping University
+Alexander Bock, Linköping University
+
+Contributors: Alexander Fridlund, Joel Kronander, Daniel Jönsson, Erik Sundén, Gene Payne
+
+For any questions or information about the project contact: alexander.bock@liu.se
+
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of
+   conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice, this list of
+   conditions and the following disclaimer in the documentation and/or other materials
+   provided with the distribution.
+3. Neither the name of the copyright holder nor the names of its contributors may be used
+   to endorse or promote products derived from this software without specific prior
+   written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+SGCT is using the following libraries:
+GLFW
+=========================================================
+Copyright © 2002-2006 Marcus Geelnard
+Copyright © 2006-2011 Camilla Berglund
+Web: http://www.glfw.org
+License: http://www.glfw.org/license.html
+
+GLM - OpenGL Mathematics
+=========================================================
+Copyright (c) 2005 - 2012 G-Truc Creation (www.g-truc.net)
+Web: http://glm.g-truc.net/
+License: http://glm.g-truc.net/copying.txt
+
+Freetype
+=========================================================
+Copyright 1996-2002, 2006 by David Turner, Robert Wilhelm, and Werner Lemberg
+Web: http://www.freetype.org
+License: http://www.freetype.org/license.html
+
+glbindings
+=========================================================
+Copyright (c) 2014-2015 Computer Graphics Systems Group at the Hasso-Plattner-Institute
+and CG Internals GmbH, Germany. 
+Web: https://github.com/cginternals/glbinding
+License: https://github.com/cginternals/glbinding/blob/master/LICENSE
+
+libpng - Portable Network Graphics library
+=========================================================
+Copyright (c) 1998-2011 Glenn Randers-Pehrson
+(Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
+(Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
+Web: http://www.libpng.org/
+License: http://www.libpng.org/pub/png/src/libpng-LICENSE.txt
+
+TinyXML-2
+=========================================================
+Original code by Lee Thomason (www.grinninglizard.com)
+Web: http://www.grinninglizard.com/tinyxml2/index.html
+
+VRPN - Virtual Reality Peripheral Network
+=========================================================
+The Virtual Reality Peripheral Network (VRPN) is public-domain software released by the
+Department of Computer Science at the University of North Carolina at Chapel Hill
+Web: http://www.cs.unc.edu/Research/vrpn/
+
+zlib
+=========================================================
+Copyright (C) 1995-2010 Jean-loup Gailly and Mark Adler
+Web: http://www.zlib.net/
+
+libjpeg-turbo
+=========================================================
+Copyright (c) 1991-1998, Thomas G. Lane.
+Modified 2002-2009 by Guido Vollbeding.
+Copyright (C) 2009-2011, 2013, D. R. Commander.
+Acknowledgement:
+"this software is based in part on the work of the Independent JPEG Group"
+Web: http://www.libjpeg-turbo.org/
+
+SGCT is optionally using the following libraries:
+OpenVR
+=========================================================
+OpenVR provides a way to interact with Virtual Reality displays without relying on a
+specific hardware vendor's SDK.
+Copyright (c) 2015, Valve Corporation
+Web: https://github.com/ValveSoftware/openvr
+License: BSD-3-Clause
+https://github.com/ValveSoftware/openvr/blob/master/LICENSE
---
+++ src/apps/SpoutScreenshot/main.cpp
@@ -0,0 +1,217 @@
+#include <sgct/commandline.h>
+#include <sgct/engine.h>
+#include <sgct/shadermanager.h>
+#include <sgct/shareddata.h>
+#include <sgct/texturemanager.h>
+#include <sgct/window.h>
+#include <sgct/utils/box.h>
+#include <GLFW/glfw3.h>
+#include <SpoutLibrary.h>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+namespace {
+    sgct::Engine* gEngine;
+
+    struct {
+        GLuint vao;
+        GLuint vbo;
+    } geometry;
+
+    bool shouldTakeScreenshot = false;
+
+    SPOUTHANDLE receiver = nullptr;
+    std::string sender;
+    unsigned int width;
+    unsigned int height;
+    bool initialized = false;
+
+    constexpr const char* vertexShader = R"(
+  #version 330 core
+
+  layout(location = 0) in vec2 vertPositions;
+
+  out vec2 uv;
+
+  void main() {
+    gl_Position = vec4(vertPositions, 0.0, 1.0);
+    uv = (vertPositions + vec2(1.0)) / vec2(2.0);
+    uv.y = 1.0 - uv.y;
+  })";
+
+    constexpr const char* fragmentShader = R"(
+  #version 330 core
+  uniform sampler2D tex;
+  in vec2 uv;
+  out vec4 color;
+  void main() { color = texture(tex, uv); }
+)";
+} // namespace
+
+using namespace sgct;
+ 
+bool bindSpout() {
+    const bool creationSuccess = receiver->CreateReceiver(sender.data(), width, height);
+    if (!initialized && creationSuccess) {
+        MessageHandler::instance()->printInfo(
+            "Spout: Initing %ux%u texture from '%s'", width, height, sender.c_str()
+        );
+        initialized = true;
+    }
+
+    if (initialized) {
+        const bool receiveSucess = receiver->ReceiveTexture(sender.data(), width, height);
+        if (receiveSucess) {
+            return receiver->BindSharedTexture();
+        }
+        else {
+            MessageHandler::instance()->printInfo("Spout disconnected");
+
+            // reset if disconnected
+            initialized = false;
+            sender = '\0';
+            receiver->ReleaseReceiver();
+        }
+
+    }
+
+    return false;
+}
+
+void drawFun() {
+    ShaderManager::instance()->bindShaderProgram("xform");
+
+    glActiveTexture(GL_TEXTURE0);
+    bindSpout();
+
+    glBindVertexArray(geometry.vao);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    ShaderManager::instance()->unBindShaderProgram();
+    receiver->UnBindSharedTexture();
+
+    if (shouldTakeScreenshot) {
+        gEngine->takeScreenshot();
+        shouldTakeScreenshot = false;
+    }
+}
+
+void initOGLFun() {
+    glGenVertexArrays(1, &geometry.vao);
+    glGenBuffers(1, &geometry.vbo);
+
+    glBindVertexArray(geometry.vao);
+    glBindBuffer(GL_ARRAY_BUFFER, geometry.vbo);
+
+    struct Vertex { float x, y; };
+
+    std::array<Vertex, 6> vertices;
+    vertices[0] = { -1.f, -1.f };
+    vertices[1] = { 1.f,  1.f };
+    vertices[2] = {-1.f, 1.f };
+    vertices[3] = { -1.f, -1.f };
+    vertices[4] = { 1.f, -1.f };
+    vertices[5] = { 1.f,  1.f };
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), nullptr);
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        vertices.size() * sizeof(Vertex),
+        vertices.data(),
+        GL_STATIC_DRAW
+    );
+
+    // setup spout
+    sender = '\0';
+    receiver = GetSpout();
+    
+    // set background
+    Engine::instance()->setClearColor(glm::vec4(0.3f, 0.3f, 0.3f, 0.f));
+    
+    TextureManager::instance()->setAnisotropicFilterSize(8.f);
+    TextureManager::instance()->setCompression(TextureManager::CompressionMode::S3TC_DXT);
+
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);
+
+    ShaderManager::instance()->addShaderProgram(
+        "xform",
+        vertexShader,
+        fragmentShader,
+        ShaderProgram::ShaderSourceType::String
+    );
+    ShaderManager::instance()->bindShaderProgram("xform");
+    const ShaderProgram& prog = ShaderManager::instance()->getShaderProgram("xform");
+
+    GLint textureLoc = prog.getUniformLocation("tex");
+    glUniform1i(textureLoc, 0);
+
+    ShaderManager::instance()->unBindShaderProgram();
+    Engine::checkForOGLErrors();
+}
+
+void cleanUpFun() {
+    glDeleteVertexArrays(1, &geometry.vao);
+    glDeleteBuffers(1, &geometry.vbo);
+
+    if (receiver) {
+        receiver->ReleaseReceiver();
+        receiver->Release();
+    }
+}
+
+void keyboardCallback(int key, int, int action, int) {
+    if (action == action::Press) {
+        switch (key) {
+            case key::Key1:
+                sender = "Right\0";
+                MessageHandler::instance()->printInfo("Settings receiver to 'Right'");
+                break;
+            case key::Key2:
+                sender = "zLeft\0";
+                MessageHandler::instance()->printInfo("Settings receiver to 'zLeft'");
+                break;
+            case key::Key3:
+                sender = "Bottom\0";
+                MessageHandler::instance()->printInfo("Settings receiver to 'Bottom'");
+                break;
+            case key::Key4:
+                sender = "Top\0";
+                MessageHandler::instance()->printInfo("Settings receiver to 'Top'");
+                break;
+            case key::Key5:
+                sender = "Left\0";
+                MessageHandler::instance()->printInfo("Settings receiver to 'Left'");
+                break;
+            case key::Key6:
+                sender = "zRight\0";
+                MessageHandler::instance()->printInfo("Settings receiver to 'zRight'");
+                break;
+            case key::Space:
+                shouldTakeScreenshot = true;
+                break;
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    std::vector<std::string> arg(argv + 1, argv + argc);
+    Configuration config = parseArguments(arg);
+    config::Cluster cluster = loadCluster(config.configFilename);
+    gEngine = new Engine(config);
+
+    gEngine->setInitOGLFunction(initOGLFun);
+    gEngine->setDrawFunction(drawFun);
+    gEngine->setCleanUpFunction(cleanUpFun);
+    gEngine->setKeyboardCallbackFunction(keyboardCallback);
+
+    if (!gEngine->init(Engine::RunMode::OpenGL_3_3_Core_Profile, cluster)) {
+        delete gEngine;
+        return EXIT_FAILURE;
+    }
+
+    gEngine->render();
+    delete gEngine;
+    exit(EXIT_SUCCESS);
+}

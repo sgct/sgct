d98c1e012d65c6535ff552d0b1ddbd94dc31703e
Alexander Bock <mail@alexanderbock.eu>
2020-01-27 07:58:12 -0500

    Fix for spherical mirror 4 meshes projection example

--- src/sgct/projection/nonlinearprojection.cpp
+++ src/sgct/projection/nonlinearprojection.cpp
@@ -257,69 +257,14 @@ void NonLinearProjection::generateMap(unsigned int& texture, unsigned int intern
 
     // set up texture target
     glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_CUBE_MAP, texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
 
     // Disable mipmaps
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_BASE_LEVEL, 0);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAX_LEVEL, 0);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
 
     glTexImage2D(
-        GL_TEXTURE_CUBE_MAP_POSITIVE_X,
-        0,
-        internalFormat,
-        _cubemapResolution,
-        _cubemapResolution,
-        0,
-        format,
-        type,
-        nullptr
-    );
-    glTexImage2D(
-        GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
-        0,
-        internalFormat,
-        _cubemapResolution,
-        _cubemapResolution,
-        0,
-        format,
-        type,
-        nullptr
-    );
-    glTexImage2D(
-        GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
-        0,
-        internalFormat,
-        _cubemapResolution,
-        _cubemapResolution,
-        0,
-        format,
-        type,
-        nullptr
-    );
-    glTexImage2D(
-        GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
-        0,
-        internalFormat,
-        _cubemapResolution,
-        _cubemapResolution,
-        0,
-        format,
-        type,
-        nullptr
-    );
-    glTexImage2D(
-        GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
-        0,
-        internalFormat,
-        _cubemapResolution,
-        _cubemapResolution,
-        0,
-        format,
-        type,
-        nullptr
-    );
-    glTexImage2D(
-        GL_TEXTURE_CUBE_MAP_NEGATIVE_Z,
+        GL_TEXTURE_2D,
         0,
         internalFormat,
         _cubemapResolution,
@@ -330,10 +275,10 @@ void NonLinearProjection::generateMap(unsigned int& texture, unsigned int intern
         nullptr
     );
 
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 }
 
 void NonLinearProjection::generateCubeMap(unsigned int& texture,

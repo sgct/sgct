39f52997082e04ffb0332f928c2f547574917581
Peter Steneteg <peter@steneteg.se>
2020-04-12 15:13:06 +0200

    Update the apps the use the new system for dependencies

--- src/apps/simplenavigation/main.cpp
+++ src/apps/simplenavigation/main.cpp
@@ -394,8 +394,8 @@ std::vector<std::byte> encode() {
     return data;
 }
 
-void decode(const std::vector<std::byte>& data, unsigned int pos) {
-    deserializeObject(data, pos, xform);
+void decode(const std::vector<std::byte>& data, unsigned int apos) {
+    deserializeObject(data, apos, xform);
 }
 
 void keyboard(Key key, Modifier, Action action, int) {

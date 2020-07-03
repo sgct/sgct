a2a5a58a1fbd7635a0557db8c9b81ba2da2bcdb1
Peter Steneteg <peter@steneteg.se>
2020-07-03 15:13:20 +0200

    Handle dependencies in cmake config file

---
+++ support/cmake/sgctConfig.cmake
@@ -0,0 +1,12 @@
+include(CMakeFindDependencyMacro)
+
+find_dependency(unofficial-tracy)
+find_dependency(glad)
+find_dependency(ZLIB)
+find_dependency(PNG)
+find_dependency(minizip)
+find_dependency(tinyxml2)
+find_dependency(freetype)
+find_dependency(glm)
+
+include("${CMAKE_CURRENT_LIST_DIR}/sgctTargets.cmake")

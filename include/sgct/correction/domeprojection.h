f5aad4fa4c9b6dad3dc91576b78a252bc90fb0bd
Alexander Bock <alexander.bock@liu.se>
2022-08-01 11:32:49 +0200

    More

--- include/sgct/correction/domeprojection.h
+++ include/sgct/correction/domeprojection.h
@@ -11,11 +11,11 @@
 
 #include <sgct/math.h>
 #include <sgct/correction/buffer.h>
-#include <string>
+#include <filesystem>
 
 namespace sgct::correction {
 
-Buffer generateDomeProjectionMesh(const std::string& path, const vec2& pos,
+Buffer generateDomeProjectionMesh(const std::filesystem::path& path, const vec2& pos,
     const vec2& size);
 
 } // namespace sgct::correction

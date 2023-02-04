df83ad7b7be6c60fefaa9b168b944a978eb356ff
Alexander Bock <alexander.bock@liu.se>
2023-02-04 16:44:11 +0100

    Better handling of spout DLLs

--- support/cmake/copy_files.cmake
+++ support/cmake/copy_files.cmake
@@ -9,8 +9,9 @@
 function (copy_files target)
   # Add the copy command
   foreach(file_i ${ARGN})
-    add_custom_command(TARGET ${target} POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different "${file_i}" $<TARGET_FILE_DIR:${target}>
+    add_custom_command(
+      TARGET ${target} POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E copy_if_different "${file_i}" $<TARGET_FILE_DIR:${target}>
   )
   endforeach ()
 endfunction ()

287e0b3b2dd96d0fcabed3528589feb0ff27fc6b
Alexander Bock <alexander.bock@liu.se>
2023-11-08 07:39:58 +0100

    Fix issue with including Tracy

--- support/cmake/register_package.cmake
+++ support/cmake/register_package.cmake
@@ -6,7 +6,7 @@ function(sgct_register_package name)
   endforeach()
   string(TOLOWER "${name}" lowercase_name)
   set(${name}_DIR "${CMAKE_BINARY_DIR}/pkg/${lowercase_name}" CACHE PATH "" FORCE)
-  file(WRITE "${CMAKE_BINARY_DIR}/pkg/${lowercase_name}/${name}Config.cmake" 
+  file(WRITE "${CMAKE_BINARY_DIR}/pkg/${lowercase_name}/${name}Config.cmake"
     "# Fake Config file for ${name}\n"
     "set(${name}_FOUND ON)\n"
     "set(${name}_LIBRARIES ${ARGN})\n"

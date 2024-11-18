13cb7cbcc67b50cfecbbdc6f1eba3aa7b7541222
Alexander Bock <alexander.bock@liu.se>
2024-11-18 14:17:01 +0100

    Remove grid rendering from calibrator

--- apps/calibrator/main.cpp
+++ apps/calibrator/main.cpp
@@ -19,16 +19,6 @@ namespace {
     struct {
         GLuint vao = 0;
         GLuint vbo = 0;
-        GLuint iboLine = 0;
-        int nVertLine = 0;
-
-        GLint mvpMatrixLocation = -1;
-        GLint cameraMatrixLocation = -1;
-    } grid;
-
-    struct {
-        GLuint vao = 0;
-        GLuint vbo = 0;
 
         GLint mvpMatrixLocation = -1;
         GLint cameraMatrixLocation = -1;
@@ -49,49 +39,10 @@ namespace {
 
     bool takeScreenshot = false;
     bool captureBackbuffer = false;
-    bool renderGrid = true;
-    bool renderBox = false;
     bool showId = false;
     bool showStats = false;
 
     float radius = 7.4f;
-
-    constexpr std::string_view GridVertexShader = R"(
-#version 330 core
-
-layout(location = 0) in vec2 in_position;
-
-out vec4 tr_color;
-
-uniform float radius;
-uniform mat4 mvp;
-uniform mat4 camera;
-
-const float PI = 3.141592654;
-const float PI_HALF = PI / 2.0;
-
-void main() {
-  float elevation = in_position[0];
-  float azimuth = in_position[1];
-
-  vec3 p = vec3(
-    radius * cos(elevation) * sin(azimuth),
-    radius * sin(elevation),
-    -radius * cos(elevation) * cos(azimuth)
-  );
-  gl_Position = mvp * camera * vec4(p, 1.0);
-  tr_color = vec4(p, 1.0);
-}
-)";
-
-    constexpr std::string_view GridFragmentShader = R"(
-#version 330 core
-
-in vec4 tr_color;
-out vec4 color;
-
-void main() { color = tr_color; }
-)";
 
     constexpr std::string_view BoxVertexShader = R"(
 #version 330 core
@@ -131,106 +82,7 @@ void main() {
 
 using namespace sgct;
 
-void initializeGrid() {
-    constexpr uint16_t RestartIndex = std::numeric_limits<uint16_t>::max();
-    glEnable(GL_PRIMITIVE_RESTART);
-    glPrimitiveRestartIndex(RestartIndex);
-
-    glGenVertexArrays(1, &grid.vao);
-    glGenBuffers(1, &grid.vbo);
-    glGenBuffers(1, &grid.iboLine);
-
-    glBindVertexArray(grid.vao);
-    glBindBuffer(GL_ARRAY_BUFFER, grid.vbo);
-
-    constexpr int ElevationSteps = 40;
-    constexpr int AzimuthSteps = 160;
-
-    struct GridVertex {
-        float elevation;
-        float azimuth;
-    };
-    std::vector<GridVertex> vertices;
-    // (abock, 2019-10-09) We generate the vertices ring-wise;  first iterating over the
-    // elevation and then the azimuth will lead to the bottom most ring be filled first,
-    // before going to the upper rings.  That also means that two vertices on top of each
-    // other should be separated in the vertices list by 'AzimuthSteps' positions
-    for (int e = 0; e <= ElevationSteps; ++e) {
-        for (int a = 0; a < AzimuthSteps; ++a) {
-            float ev = static_cast<float>(e) / static_cast<float>(ElevationSteps - 1);
-            float av = static_cast<float>(a) / static_cast<float>(AzimuthSteps - 1);
-            
-            GridVertex vertex;
-            vertex.elevation = glm::radians(ev * 90.f);
-            vertex.azimuth = glm::radians(av * 360.f);
-            vertices.push_back(vertex);
-        }
-    }
-
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(GridVertex), nullptr);
-    glBufferData(
-        GL_ARRAY_BUFFER,
-        vertices.size() * sizeof(GridVertex),
-        vertices.data(),
-        GL_STATIC_DRAW
-    );
-
-    {
-        // Line representation
-        // (abock, 2019-10-09) It's possible to compute a better size of how many indices
-        // we'll need, but I can't be asked (and we are seriously not performance limited)
-        std::vector<uint16_t> indices;
-        indices.reserve(2 * ElevationSteps * AzimuthSteps);
-
-        // First the horizontal, azimuth lines
-        for (int e = 0; e < ElevationSteps; ++e) {
-            std::vector<uint16_t> t(AzimuthSteps);
-            std::iota(t.begin(), t.end(), static_cast<uint16_t>(e * AzimuthSteps));
-            t.push_back(static_cast<uint16_t>(e * AzimuthSteps)); // close the ring
-            t.push_back(RestartIndex); // restart for the next ring
-            indices.insert(indices.end(), t.begin(), t.end());
-        }
-        indices.push_back(RestartIndex);
-        // Then the vertical lines; see above; every vertical vertex is separated by
-        // exactly 'AzimuthSteps' positions in the vertex array
-        for (int a = 0; a < AzimuthSteps; ++a) {
-            for (int e = 0; e < ElevationSteps; ++e) {
-                indices.push_back(static_cast<uint16_t>(a + e * AzimuthSteps));
-            }
-            indices.push_back(RestartIndex);
-        }
-
-        grid.nVertLine = static_cast<int>(indices.size());
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, grid.iboLine);
-        glBufferData(
-            GL_ELEMENT_ARRAY_BUFFER,
-            indices.size() * sizeof(uint16_t),
-            indices.data(),
-            GL_STATIC_DRAW
-        );
-    }
-    glBindVertexArray(0);
-
-
-    ShaderManager::instance().addShaderProgram(
-        "grid",
-        GridVertexShader,
-        GridFragmentShader
-    );
-    const ShaderProgram& prog = ShaderManager::instance().shaderProgram("grid");
-    prog.bind();
-    grid.mvpMatrixLocation = glGetUniformLocation(prog.id(), "mvp");
-    assert(grid.mvpMatrixLocation != -1);
-    grid.cameraMatrixLocation = glGetUniformLocation(prog.id(), "camera");
-    assert(grid.cameraMatrixLocation != -1);
-    GLuint radiusLocation = glGetUniformLocation(prog.id(), "radius");
-    assert(radiusLocation != std::numeric_limits<GLuint>::max());
-    glUniform1f(radiusLocation, radius);
-    prog.unbind();
-}
-
-void initializeBox() {
+void initGL(GLFWwindow*) {
     glGenVertexArrays(1, &box.vao);
     glGenBuffers(1, &box.vbo);
     glBindVertexArray(box.vao);
@@ -445,11 +297,6 @@ void initializeBox() {
     prog.unbind();
 }
 
-void initGL(GLFWwindow*) {
-    initializeGrid();
-    initializeBox();
-}
-
 void postSyncPreDraw() {
     Settings::instance().setCaptureFromBackBuffer(captureBackbuffer);
     Engine::instance().setStatsGraphVisibility(showStats);
@@ -476,39 +323,26 @@ void draw(const RenderData& data) {
     glm::vec3 up = glm::cross(right, direction);
     const glm::mat4 c = glm::lookAt(glm::vec3(0.f), direction, up);
 
-    if (renderGrid) {
-        ShaderManager::instance().shaderProgram("grid").bind();
-        glUniformMatrix4fv(grid.mvpMatrixLocation, 1, GL_FALSE, mvp.values);
-        glUniformMatrix4fv(grid.cameraMatrixLocation, 1, GL_FALSE, glm::value_ptr(c));
-        glBindVertexArray(grid.vao);
-        glDrawElements(GL_LINE_STRIP, grid.nVertLine, GL_UNSIGNED_SHORT, nullptr);
-        glBindVertexArray(0);
-        ShaderManager::instance().shaderProgram("grid").unbind();
-    }
+    ShaderManager::instance().shaderProgram("box").bind();
+    glUniformMatrix4fv(box.mvpMatrixLocation, 1, GL_FALSE, mvp.values);
+    glUniformMatrix4fv(box.cameraMatrixLocation, 1, GL_FALSE, glm::value_ptr(c));
+    glBindVertexArray(box.vao);
 
-    if (renderBox) {
-        ShaderManager::instance().shaderProgram("box").bind();
-        glUniformMatrix4fv(box.mvpMatrixLocation, 1, GL_FALSE, mvp.values);
-        glUniformMatrix4fv(box.cameraMatrixLocation, 1, GL_FALSE, glm::value_ptr(c));
-        glBindVertexArray(box.vao);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, box.textureFront);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, box.textureRight);
+    glActiveTexture(GL_TEXTURE2);
+    glBindTexture(GL_TEXTURE_2D, box.textureBack);
+    glActiveTexture(GL_TEXTURE3);
+    glBindTexture(GL_TEXTURE_2D, box.textureLeft);
+    glActiveTexture(GL_TEXTURE4);
+    glBindTexture(GL_TEXTURE_2D, box.textureTop);
+    glActiveTexture(GL_TEXTURE5);
+    glBindTexture(GL_TEXTURE_2D, box.textureBottom);
 
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, box.textureFront);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, box.textureRight);
-        glActiveTexture(GL_TEXTURE2);
-        glBindTexture(GL_TEXTURE_2D, box.textureBack);
-        glActiveTexture(GL_TEXTURE3);
-        glBindTexture(GL_TEXTURE_2D, box.textureLeft);
-        glActiveTexture(GL_TEXTURE4);
-        glBindTexture(GL_TEXTURE_2D, box.textureTop);
-        glActiveTexture(GL_TEXTURE5);
-        glBindTexture(GL_TEXTURE_2D, box.textureBottom);
-
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-        glBindVertexArray(0);
-        ShaderManager::instance().shaderProgram("grid").unbind();
-    }
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    glBindVertexArray(0);
 }
 
 void draw2D(const RenderData& data) {
@@ -563,11 +397,7 @@ void postDraw() {
         constexpr int FrameGridFirstScreenshot = 5;
         constexpr int FrameGridSettingBackBuffer = 10;
         constexpr int FrameGridSecondScreenshot = 15;
-        constexpr int ChangeToBox = 20;
-        constexpr int FrameBoxFirstScreenshot = 25;
-        constexpr int FrameBoxSettingBackBuffer = 30;
-        constexpr int FrameBoxSecondScreenshot = 35;
-        constexpr int FrameTerminate = 40;
+        constexpr int FrameTerminate = 20;
 
         switch (frameNumber) {
             case FrameGridFirstScreenshot:
@@ -588,31 +418,6 @@ void postDraw() {
             case FrameGridSecondScreenshot + 2:
                 std::this_thread::sleep_for(std::chrono::milliseconds(500));
                 break;
-            case ChangeToBox:
-                Log::Info("Setting to capture from front buffer");
-                Log::Info("Changing the rendering to the test box");
-                captureBackbuffer = false;
-                renderGrid = false;
-                renderBox = true;
-                break;
-            case FrameBoxFirstScreenshot:
-                Log::Info("Setting to take first box screenshot");
-                takeScreenshot = true;
-                break;
-            case FrameBoxFirstScreenshot + 2:
-                std::this_thread::sleep_for(std::chrono::milliseconds(500));
-                break;
-            case FrameBoxSettingBackBuffer:
-                Log::Info("Setting to capture from Back buffer");
-                captureBackbuffer = true;
-                break;
-            case FrameBoxSecondScreenshot:
-                Log::Info("Setting to take second box screenshot");
-                takeScreenshot = true;
-                break;
-            case FrameBoxSecondScreenshot + 2:
-                std::this_thread::sleep_for(std::chrono::milliseconds(500));
-                break;
             case FrameTerminate:
                 std::this_thread::sleep_for(std::chrono::seconds(5));
                 Engine::instance().terminate();
@@ -627,8 +432,6 @@ std::vector<std::byte> encode() {
     std::vector<std::byte> data;
     serializeObject(data, takeScreenshot);
     serializeObject(data, captureBackbuffer);
-    serializeObject(data, renderGrid);
-    serializeObject(data, renderBox);
     serializeObject(data, runTests);
     serializeObject(data, frameNumber);
     serializeObject(data, showId);
@@ -642,8 +445,6 @@ void decode(const std::vector<std::byte>& data) {
     unsigned pos = 0;
     deserializeObject(data, pos, takeScreenshot);
     deserializeObject(data, pos, captureBackbuffer);
-    deserializeObject(data, pos, renderGrid);
-    deserializeObject(data, pos, renderBox);
     deserializeObject(data, pos, runTests);
     deserializeObject(data, pos, frameNumber);
     deserializeObject(data, pos, showId);
@@ -653,10 +454,6 @@ void decode(const std::vector<std::byte>& data) {
 }
 
 void cleanup() {
-    glDeleteVertexArrays(1, &grid.vao);
-    glDeleteBuffers(1, &grid.vbo);
-    glDeleteBuffers(1, &grid.iboLine);
-
     TextureManager::instance().removeTexture(box.textureFront);
     TextureManager::instance().removeTexture(box.textureRight);
     TextureManager::instance().removeTexture(box.textureBack);
@@ -666,11 +463,6 @@ void cleanup() {
 }
 
 void keyboard(Key key, Modifier, Action action, int, Window*) {
-    if (key == Key::Space && action == Action::Press) {
-        renderGrid = !renderGrid;
-        renderBox = !renderBox;
-    }
-
     if (key == Key::Enter && action == Action::Press) {
         frameNumber = 0;
         runTests = true;
